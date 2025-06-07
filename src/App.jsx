a1e25b67a2a87f6c6f8315f2b7377a8abfda2d47
Sam Phelps <samueldphelps@gmail.com>
2025-06-07 14:59:47 -0400

    Added default export to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
 // Placeholder for main App component
+export default App;
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Your content goes here */}
+    </div>
+  );
+}
+
+export default App;
